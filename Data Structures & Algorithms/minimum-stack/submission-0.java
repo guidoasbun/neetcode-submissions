1e0f0fc04575d589add44df7afdbd69307e7516f
Guido Asbun <guido@asbun.io>
2026-05-29 16:59:10 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,50 @@
+class MinStack {
+    // create a Node that holds the 3 values
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        // Constructor for the Node
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+    // Initialize the Node
+    private Node head;
+
+    public MinStack() {
+        head = null;
+    }
+    
+    public void push(int val) {
+        if (head == null) head = new Node(val, val, null);
+        else head = new Node(val, Math.min(val, head.min), head);
+    }
+    
+    public void pop() {
+        head = head.next;
+    }
+    
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return head.min;
+    }
+
+    
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */

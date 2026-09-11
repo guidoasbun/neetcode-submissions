91f7edcd31f8bc1c005adc31efaf25f8e89f30ba
Guido Asbun <guido@asbun.io>
2026-09-11 14:20:50 -0700

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        if (nums.length == 0) return false;
+
+        HashSet<Integer> seenNums = new HashSet<>();
+        
+        for (int num : nums) {
+            if (seenNums.contains(num)) return true;
+            seenNums.add(num);
+        }
+        return false;
+    }
+}

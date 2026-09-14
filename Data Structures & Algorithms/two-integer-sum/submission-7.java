24dfff8ea85d63cbed9192380d2484899a99fcac
Guido Asbun <guido@asbun.io>
2026-09-14 13:19:57 -0700

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //if (nums.length == 0 || target == 0) return new int[] {};
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int index = 0; index < nums.length; index++) {
+            int complement = target - nums[index];
+
+            if (map.containsKey(complement)) return new int[] {map.get(complement), index};
+
+            map.put(nums[index], index);
+        }
+
+        return new int[] {};
+    }
+}
